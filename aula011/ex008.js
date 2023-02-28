8d54b1bdb7bbe19a8684c8f255072675b334f2af
Arthur Schneider <ruhtra4.5@gmail.com>
2023-02-28 10:24:28 -0300

    Criei os ex008 e 009

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,7 @@
-console.log('O conselo funcionou corretamente!')
+var vel = 70
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`) //Quando for exibir uma var tem que estar dentro de crase ``
+if(vel > 60){ //Condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Utilize sempre o cinto de segurança!')//Exibe na tela
