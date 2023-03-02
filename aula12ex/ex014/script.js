ca480e7e200a3a3051abb42decc235be2401115e
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-02 13:59:45 -0300

    Finalizei o ex 014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,19 +3,23 @@ function carregar(){
     var hora = agora.getHours()
     var horadia = window.document.querySelector('div#hora')
     var img = window.document.querySelector('img#img')
-
+    var msg = window.document.getElementById('msg')
+    
     horadia.innerHTML = `Agora são ${hora} horas`
-
+    
     if(hora >= 6 && hora < 12){
     //Bom dia
-        img.src = 'imagens/manha.jpg'
+        msg.innerHTML = 'Bom Dia!'
+        img.src = 'imagens/manha.jpg' //muda o src do arquivo com o id img
         document.body.style.background = '#e2cd9f'
     } else if(hora >= 12 && hora < 18){
     //Boa tarde
+        msg.innerHTML = 'Boa Tarde!'
         img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#b9846f'
     } else{
     //Boa noite
+        msg.innerHTML = 'Boa Noite!'
         img.src = 'imagens/noite.jpg'
         document.body.style.background = '#515154'
     }
