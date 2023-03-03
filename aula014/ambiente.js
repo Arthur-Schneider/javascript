2c1703f613340a22011356e289496c79e5feeb9c
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-03 16:29:14 -0300

    Ex 014 sobre for

--- aula014/ambiente.js
+++ aula014/ambiente.js
@@ -1,7 +1,8 @@
+console.log('Vai começar...')
 for(var c = 1; c <= 10; c++){
     console.log(`Passo ${c}`)
 }
-
+console.log('FIM!')
 //Define a var c com valor 1
 //Vai repetir enquanto c for menor ou igual a 10
 //Adiciona +1 a var c a cada repetição
