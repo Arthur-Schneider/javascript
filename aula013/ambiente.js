2b485aef3ad022782de237abd81bb0e10d39c2bd
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-03 15:56:36 -0300

    Criei o ex 03 sobre while

---
+++ aula013/ambiente.js
@@ -0,0 +1,11 @@
+var c =1
+
+/* while(c <= 100){ //Vai repetir enquanto c for menor ou igual a 100
+    console.log(`Passo ${c}`)
+    c = c + 1 //Soma 1 a cada vez que o bloco repete (poderia ser escrito c++)
+} */
+
+do{
+    console.log(`Passo ${c}`)
+    c = c + 1 //Soma 1 a cada vez que o bloco repete (poderia ser escrito c++)
+}while(c <= 6)
