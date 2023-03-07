8547690d5eb8f828423d7596dabf4c3f478a21ec
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-07 16:16:57 -0300

    ex 017 finlalizado

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,11 +1,17 @@
 function tabuada(){
     var numero = window.document.querySelector('input#num')
-    var num = Number(numero.value)
     var res = window.document.querySelector('textarea#res')
-    var resultado = 0
+    var lista = window.document.getElementById('lista')
 
-    for(var controle = 1; controle <= 10; controle++){
-        resultado = controle*num
-        res.innerHTML += (`${num} x ${controle} = ${resultado} `)
+    lista.innerHTML = '' //limpa o select
+    if(numero.value.length == 0){
+        window.alert('Dígite um número!')
+    }else{
+        for(var controle = 1; controle <= 10; controle++){
+            var num = Number(numero.value)
+            var item = document.createElement('option') //cria o elemento option
+            item.text = `${num} x ${controle} = ${num*controle} `//adiciona o text no option
+            lista.appendChild(item) //adiciona o item como filho do select
+        }
     }
 }
