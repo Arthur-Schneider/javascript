9e7fbaf9f1e4350c4f858b520f2fc7ce305fd177
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-07 14:54:22 -0300

    ex 016 finalizado

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -7,21 +7,41 @@ function contar(){
     var passo = Number(dadopasso.value)
     var res = window.document.getElementById('resultado')
     
-    res.innerHTML = ('<p><strong>Contando...</strong></p>')
+    res.innerHTML = ('<p><strong>Contando:</strong></p>')
     
-    while(inicio <= final){
-        if(passo == 0){
-            window.alert('Passo inválido! CONSIDERANDO PASSO 1')
-            passo = 1
+    if(dadoinicio.value.length == 0 || dadofinal.value.length == 0){ //testa se algum campo ficou vazio
+        res.innerHTML = ('[ERRO] FALTAM DADOS, DIGITE NOVAMENTE!')
+    }else{ //executa se todos os campos foram digitados
+        if(inicio < final){ // se o numero inicial for menor que o final, ordem CRESCENTE    
+            while(inicio <= final){
+                if(passo == 0){
+                    window.alert('Passo inválido! CONSIDERANDO PASSO 1')
+                    passo = 1
+                }
+                res.innerHTML += (inicio)
+                if(inicio < final){
+                    res.innerHTML += ('👉')
+                }else{
+                    res.innerHTML +=('🏁')
+                }
+                
+                inicio = inicio + passo
+            }
+        }else{ //se o numero inicial for maior que o final, ordem DECRESCENTE
+            while(inicio >= final){
+                if(passo == 0){
+                    window.alert('Passo inválido! CONSIDERANDO PASSO 1')
+                    passo = 1
+                }
+                res.innerHTML += (inicio)
+                if(inicio > final){
+                    res.innerHTML += ('👉')
+                }else{
+                    res.innerHTML +=('🏁')
+                }
+                
+                inicio = inicio - passo
+            }
         }
-        res.innerHTML += (inicio)
-        if(inicio < final){
-            res.innerHTML += ('👉')
-        }else{
-            res.innerHTML +=('🏁')
-        }
-        
-        inicio = inicio + passo
     }
-    
 }
