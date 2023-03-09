c19b75c7d4de5376327a93b8b62ac0fe7ed94e35
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-09 10:17:18 -0300

    continuação do ex018

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -1,18 +1,37 @@
-var txtnum = window.document.querySelector('input#num')
+var vetor = []
+var pos = 0
 
 function carregar(){
-    
+    var txtnum = window.document.querySelector('input#num')
+    var num = Number(txtnum.value)//não vai aspas('input#num')
     var lista = window.document.getElementById('lista')
     var item = document.createElement('option')
-    var num = Number(txtnum.value)//não vai aspas
-    var vetor = []
-    vetor.push(num)
-    item.text = `Valor ${vetor} adicionado.`
-    lista.appendChild(item)
-   
-        
+    
+    if(txtnum.value.length == 0){
+        window.alert('Dígite um número!')
+    }else if(num >= 1 && num <= 100){
+        vetor.push(num)
+        item.text = `Valor ${vetor[pos]} adicionado.`
+        lista.appendChild(item)
+        pos++
+    }else{
+        window.alert('Dígite um número entre 1 e 100!')
+    }
 }
 function finalizar(){
     var res = window.document.getElementById('res')
-    res.innerHTML = (`Ao todo temos 7 números cadastrados`)
+    var tamanho = vetor.length
+    var soma = 0
+    for(let pos in vetor){
+        soma = soma + vetor[pos]
+    }
+    var media = soma / pos
+
+    if(tamanho == 0){
+        window.alert('Dígite um número!')
+    }else{
+    res.innerHTML = (`Ao todo temos ${tamanho} números cadastrados <br>`)
+    res.innerHTML += (` A soma de todos os números é ${soma} <br>`)
+    res.innerHTML += (`A média dos números é ${media}`)
+    }
 }
