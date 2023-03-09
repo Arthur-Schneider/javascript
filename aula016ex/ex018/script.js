cc9fc58ea5908c30715062b6ad33c943774b6802
Arthur Schneider <ruhtra4.5@gmail.com>
2023-03-09 18:13:17 -0300

    Finalizei o ex 018

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -1,14 +1,22 @@
+var txtnum = window.document.querySelector('input#num')
+var lista = window.document.getElementById('lista')
 var vetor = []
 var pos = 0
 
+function inlista(n, l){
+    if(l.indexOf(n) != -1){
+        return true
+    }else{
+        return false
+    } 
+}
+
 function carregar(){
-    var txtnum = window.document.querySelector('input#num')
     var num = Number(txtnum.value)//não vai aspas('input#num')
-    var lista = window.document.getElementById('lista')
     var item = document.createElement('option')
-    
-    if(txtnum.value.length == 0){
-        window.alert('Dígite um número!')
+    res.innerHTML = ''
+    if(inlista(num, vetor) == true){
+        window.alert('Este número já está na lista!')
     }else if(num >= 1 && num <= 100){
         vetor.push(num)
         item.text = `Valor ${vetor[pos]} adicionado.`
@@ -17,21 +25,36 @@ function carregar(){
     }else{
         window.alert('Dígite um número entre 1 e 100!')
     }
+    txtnum.value = '' //apaga a caixa de texto
+    txtnum.focus() //deixa o cursor selecionado na caixa de texto
 }
-function finalizar(){
-    var res = window.document.getElementById('res')
-    var tamanho = vetor.length
-    var soma = 0
-    for(let pos in vetor){
-        soma = soma + vetor[pos]
-    }
-    var media = soma / pos
 
+function finalizar(){
+    
+    
+    
     if(tamanho == 0){
-        window.alert('Dígite um número!')
+        window.alert('Adicione valores antes de finalizar!')
     }else{
-    res.innerHTML = (`Ao todo temos ${tamanho} números cadastrados <br>`)
-    res.innerHTML += (` A soma de todos os números é ${soma} <br>`)
-    res.innerHTML += (`A média dos números é ${media}`)
+        var res = window.document.getElementById('res')
+        var tamanho = vetor.length
+        var min = vetor[0]
+        var max = vetor[0]
+        var soma = 0
+        for(let pos in vetor){
+            soma = soma + vetor[pos] //soma todos os valores
+            if(vetor[pos] < min){ //verifica qual o menor valor do vetor
+                min = vetor[pos]
+            }else if(vetor[pos] > max){ //verifica qual o maior valor do vetor
+                max = vetor[pos]
+            }
+        }
+        var media = soma / pos //calcula a média dos valores
+        res.innerHTML = (`Ao todo temos <strong>${tamanho}</strong> números cadastrados <br>`)
+        res.innerHTML += (` O menor número é o <strong>${min}</strong> <br>`)
+        res.innerHTML += (` O maior número é o <strong>${max}</strong> <br>`)
+        res.innerHTML += (` A soma de todos os números é <strong>${soma}</strong> <br>`)
+        res.innerHTML += (`A média dos números é <strong>${media}</strong>`)
+        
     }
 }
